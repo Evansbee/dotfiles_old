cfe308efdb5a4fb07c5b1158d3b6bfcb862e816d
Evan Ackmann <evansbee@gmail.com>
2011-10-04 21:15:00 -0400

    updated config

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -12,8 +12,8 @@ require("naughty")
 beautiful.init("/home/evansbee/.config/awesome/themes/evanstheme/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
-editor = os.getenv("EDITOR") or "nano"
+terminal = "urxvt"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
@@ -28,16 +28,16 @@ layouts =
 {
     awful.layout.suit.floating,
     awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier
+--  awful.layout.suit.tile.left,
+--  awful.layout.suit.tile.bottom,
+--  awful.layout.suit.tile.top,
+--  awful.layout.suit.fair,
+--  awful.layout.suit.fair.horizontal,
+--  awful.layout.suit.spiral,
+--  awful.layout.suit.spiral.dwindle,
+--  awful.layout.suit.max,
+--  awful.layout.suit.max.fullscreen,
+--  awful.layout.suit.magnifier
 }
 -- }}}
 
